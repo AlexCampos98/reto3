8973b3ae8de2c1826af7a46948187c831c647d73
AlexCampos98 <72565364+AlexCampos98@users.noreply.github.com>
2021-02-23 18:02:56 +0100

    Agregar archivos de proyecto.

---
+++ reto3/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Windows;
+
+
+namespace reto3
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnVisualizar_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxButton messageBoxButton = MessageBoxButton.OK;
+            MessageBoxImage messageBoxImage = MessageBoxImage.Error;
+            if (rbOk.IsChecked == true)
+            {
+                messageBoxButton = MessageBoxButton.OK;
+            } else if (rbOkCancel.IsChecked == true)
+            {
+                messageBoxButton = MessageBoxButton.OKCancel;
+            } else if(rbYesNo.IsChecked == true)
+            {
+                messageBoxButton = MessageBoxButton.YesNo;
+            } else if(rbYesNoCancel.IsChecked == true)
+            {
+                messageBoxButton = MessageBoxButton.YesNoCancel;
+            }
+
+            if(rbInformacion.IsChecked == true)
+            {
+                messageBoxImage = MessageBoxImage.Information;
+            } else if(rbExclamacion.IsChecked == true)
+            {
+                messageBoxImage = MessageBoxImage.Exclamation;
+            } else if(rbPregunta.IsChecked == true)
+            {
+                messageBoxImage = MessageBoxImage.Question;
+            } else if(rbError.IsChecked == true)
+            {
+                messageBoxImage = MessageBoxImage.Error;
+            }
+
+            String mensaje = tbMensaje.Text;
+            String titulo = tbTitulo.Text;
+
+            MessageBoxResult resultado = MessageBox.Show(mensaje,titulo,messageBoxButton,messageBoxImage);
+            tbResultado.Content = "Has seleccionado " + resultado;
+            tbResultado.Visibility = Visibility.Visible;
+        } 
+    }
+}
